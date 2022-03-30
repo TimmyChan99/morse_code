fd44da2e445c874d0ac91a845f2f132c8f22ef3a
Fatima Ezzahra <felemenoun@gmail.com>
2022-03-30 11:28:02 +0000

    Add linters

--- decode.rb
+++ decode.rb
@@ -1,54 +1,51 @@
 @morse_code = {
-	".-" => "a",
-	"-..." => "b",
-	"-.-." => "c",
-  "-.." => "d",
-  "." => "e",
-  "..-." => "f",
-  "--." => "g",
-  "...." => "h",
-  ".." => "i",
-  ".---" => "j",
-  "-.-" => "k",
-  ".-.." => "l",
-  "--" => "m",
-  "-." => "n",
-  "---" => "o",
-  ".--." => "p",
-  "--.-" => "q",
-  ".-." => "r",
-  "..." => "s",
-  "-" => "t",
-  "..-" => "u",
-  "...-" => "v",
-  ".--" => "w",
-  "-..-" => "x",
-  "-.--" => "y",
-  "--.." => "z",
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z'
 }
 
-def decode_char (char) 
-return @morse_code[char].upcase
+def decode_char(char)
+  @morse_code[char].upcase
 end
 
-
-def decode_word (word) 
-	trs_word = ""
-	 word.split(" ").each {
-		 |char| 
-		 trs_word.concat(decode_char(char))
-	 }
-	 return trs_word
+def decode_word(word)
+  trs_word = ''
+  word.split.each do |char|
+    trs_word.concat(decode_char(char))
+  end
+  trs_word
 end
 
 def decode(phrase)
-  trs_phrase = ""
-  phrase.split("   ").each {
-    |word|
-    trs_phrase.concat(decode_word(word)).concat(" ")
-  }
-  return trs_phrase
+  trs_phrase = ''
+  phrase.split('   ').each do |word|
+    trs_phrase.concat(decode_word(word)).concat(' ')
+  end
+  trs_phrase
 end
 
-puts decode("      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 # A BOX FULL OF RUBIES
